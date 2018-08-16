9fb689cb7759d5d354fe99e9b345cbe6103caca4
Wuge-Ran <15208219011@163.com>
2018-08-16 23:59:43 +0800

    vue

---
+++ 0816/src/server/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+let t = axios.create({
+    baseURL:'https://cnodejs.org/api/v1'
+})
+
+function getTopics(params){
+    let defaults={
+        page:10,
+        tab :'',
+        limit :10,
+        ...params
+    }
+    return  t.get('/topics',{params:defaults})
+} 
+
+function getTopicPage(id){
+    return  t.get(`/topics/${id}`)
+}
+
+export default {
+    install(Vue){
+        Vue.prototype.$ = {
+            getTopics,
+            getTopicPage
+        }
+    }
+}
