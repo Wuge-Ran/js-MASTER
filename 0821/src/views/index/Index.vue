bfd398fa5a352ba5055515298a98a25c2e0498aa
Wuge-Ran <15208219011@163.com>
2018-08-21 21:52:09 +0800

    vue

---
+++ 0821/src/views/index/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <x-header>
+      <span>夏侯商场</span>
+      <x-icon slot="overwrite-left" type="navicon" size="35" style="fill:#fff;position:relative;top:-8px;left:-3px;"></x-icon>
+      <x-icon  slot="right" type="ios-search-strong" size="30" slot='right'></x-icon>
+    </x-header>
+</template>
+
+<script>
+import { XHeader } from 'vux'
+
+export default {
+  components: {
+    XHeader
+  }
+}
+</script>
+<style>
+  @import url(./index.css);
+</style>
