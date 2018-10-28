93b63f7802dfac8733a4a2b04771f594d56288bf
Wuge-Ran <15208219011@163.com>
2018-10-28 22:33:23 +0800

    TS

--- typescript/hello.js
+++ typescript/hello.js
@@ -1,6 +1,8 @@
 "use strict";
-var abc = /** @class */ (function () {
-    function abc() {
-    }
-    return abc;
-}());
+var i = 12;
+i = '12';
+function abc(a, b, c) {
+    if (c === void 0) { c = 233; }
+    console.log(a, b, c);
+}
+abc(12, 3);
--- typescript/hello.ts
+++ typescript/hello.ts
@@ -1,5 +1,6 @@
-class abc{
-    constructor(){
-        
-    }
+let i : any = 12;
+i = '12';
+function abc(a:number,b:number,c:number=233){
+    console.log(a,b,c)
 }
+abc(12,3);
