19cb563a20ee3a5753ebebc757da3a9b8bfef243
Wuge-Ran <15208219011@163.com>
2018-10-29 23:21:55 +0800

    typeScript

--- typescript/hello.js
+++ typescript/hello.js
@@ -6,3 +6,19 @@ function abc(a, b, c) {
     console.log(a, b, c);
 }
 abc(12, 3);
+function getStock() {
+    return {
+        code: 'apple',
+        price: 135
+    };
+}
+var _a = getStock(), code = _a.code, price = _a.price;
+console.log(code, price);
+var arr1 = [1, 2, 3, 4, 5];
+var number1 = arr1[0], number2 = arr1[2];
+console.log(number1, number2); //print:1 3
+var arr2 = [99, 23, 12, 76, 35];
+for (var val of arr2) {
+    if(val <15) break;
+    console.log(val);
+}
--- typescript/hello.ts
+++ typescript/hello.ts
@@ -4,3 +4,18 @@ function abc(a:number,b:number,c:number=233){
     console.log(a,b,c)
 }
 abc(12,3);
+function getStock(){
+    return {
+        code:'apple',
+        price:135
+    }
+}
+let {code,price} = getStock();
+console.log(code,price);
+let arr1 = [1,2,3,4,5];
+let [number1,,number2] = arr1;
+console.log(number1,number2);//print:1 3
+let arr2 = [99,23,12,76,35];
+for(let val in arr2){
+    console.log(val);
+}
